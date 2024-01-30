1e4de38cb00085da8b8238e19aedd98e13570b1a
gawgavv <paramartasukma@gmail.com>
2024-01-30 17:05:59 +0700

    Chore: Configuring database and ORM
    This project is using postgresql as its database and typeorm as its ORM.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: `postgres`,
+      url: process.env.NODE_ENV === `production`? process.env.DB_PROD_URI : process.env.DB_DEV_URI,
+      entities: [],
+      logNotifications: true
+    })
   ]
 })
 export class AppModule {}
